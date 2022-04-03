5ce80b452fd1c1a57ea9be57285dab87c878de44
dheeraj231 <dheeraj7411610616@gmail.com>
2022-04-03 12:47:38 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5f7115fd-1bb2-46b2-84f6-b07053455d02';
+export const MODAL_CLOSE_SEC = 2.5;
